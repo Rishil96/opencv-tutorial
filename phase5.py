21bdfcaa0511896e93ec29d0577724d10b4b5adf
Rishil Ramesh <rishil.ramesh@outlook.com>
2025-09-09 19:39:28 +0530

    Phase 5 complete

--- phase5.py
+++ phase5.py
@@ -1,13 +1,27 @@
-# Phase 5
 import cv2
+import numpy as np
 
-# Gaussian Blur
+# 1. Read image
 image = cv2.imread("python-image.jpg")
+cv2.imshow("Original Image", image)
 
-gaussian_blurred_image = cv2.GaussianBlur(image, (7, 7), 3)
+# 2. Gaussian Blur
+gaussian_blur = cv2.GaussianBlur(image, (7, 7), 0)
+cv2.imshow("Gaussian Blur", gaussian_blur)
 
-cv2.imshow("OG", image)
-cv2.imshow("Gaussian Blur", gaussian_blurred_image)
+# 3. Median Blur
+median_blur = cv2.medianBlur(image, 7)
+cv2.imshow("Median Blur", median_blur)
+
+# 4. Sharpening
+kernel = np.array([
+    [0, -1, 0],
+    [-1, 5, -1],
+    [0, -1, 0]
+])
+sharpened = cv2.filter2D(image, -1, kernel)
+cv2.imshow("Sharpened Image", sharpened)
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
